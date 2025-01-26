b464c3fbe8dbe4a862ac12d1a48ac0fcc493306f
qhgill <qhgill2@gmail.com>
2025-01-25 17:25:58 -0800

    routing stuff

--- src/app/deck/page.tsx
+++ src/app/deck/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { getServerSession } from "next-auth/next";
 import { handler } from "@/app/api/auth/[...nextauth]/route";
 
-const Dashboard = async () => {
+const Deck = async () => {
   // Get the session on the server
   const session = await getServerSession(handler);
 
@@ -10,20 +10,22 @@ const Dashboard = async () => {
     // If no session, show a message or redirect the user
     return (
       <div>
-        <h1>You need to be logged in to view the dashboard</h1>
-        <a href="/login">Go to Login</a>
+        <h1>You need to be logged in to view the deck</h1>
+        <a className="text-lg p-4 bg-blue-400" href="/login">
+          Go to Login
+        </a>
       </div>
     );
   }
 
   return (
     <div>
-      <h1>Welcome to your dashboard, {session.user.name}!</h1>
-      <Link className="text-lg text-bold bg-blue-400 p-3" href="/deck">
-        go to deck
+      <h1>Welcome to your deck, {session.user.name}!</h1>
+      <Link className="text-lg text-bold bg-blue-400 p-3" href="/dashboard">
+        go to dashboard
       </Link>
     </div>
   );
 };
 
-export default Dashboard;
+export default Deck;
